0594d108ad36f3d1b0eacfe0693e445df83a67b6
pussinboots <pussinboots666@googlemail.com>
2012-12-05 22:34:54 +0100

    - implements charting for the report data.

--- public/js/zanox/controllers.js
+++ public/js/zanox/controllers.js
@@ -65,16 +65,26 @@ function ProgramApplicationsCtrl($scope, $rootScope, ProgramApplication) {
 	$scope.programApplications = ProgramApplication.query();
 }
 
-function MyCtrl($scope) {
+function MyCtrl($scope, Report) {
+    $scope.report = Report.query();
+    $scope.data = [];
+    $scope.report = Report.query({},{},
+	    function(result) {
+	      console.log(result);
+	       angular.forEach(result.reportItems.reportItem, function(report) {
+	      	$scope.data.push({label:report.month, value:report.total.clickCount, color:'#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)});
+	      })
+	    });
+   
     
     //usage stats for 05/2012 from http://www.w3schools.com/browsers/browsers_stats.asp
-    $scope.data = [
+    /*$scope.data = [
         {label:'Chrome', value:39.3, color:'#39E639'},
         {label:'Firefox', value:35.2, color:'#FFA640'},
         {label:'IE', value:18.1, color:'#3CA0D0'},
         {label:'Safari', value:4.3, color:'#A60000'},
         {label:'Opera', value:2.2, color:'#BF3030'}
-    ];
+    ];*/
 }
 
 function ProductDetailCtrl($scope, $routeParams, Products) {
--- public/js/zanox/services.js
+++ public/js/zanox/services.js
@@ -51,7 +51,16 @@ angular.module('productServices', ['ngResource'], function($provide) {
 		    resource = TokenHandler.wrapSignatureActions( resource, ["query"] , ["GET/programapplications/"]);
 
   			return resource;
-		});	
+		});
+		$provide.factory('Report', function($resource, TokenHandler){
+		    var resource = $resource('/assets/proxy/https://api.zanox.com/json/2011-03-01/reports/basic/' , {}, {
+		    	query : {method:'GET', params:{currency:'EUR',groupby:'month',fromdate:'2012-01-01', todate:'2012-12-02'}, isArray:false, encoding:true},
+		    });
+		    
+		    resource = TokenHandler.wrapSignatureActions( resource, ["query"] , ["GET/reports/basic/"]);
+
+  			return resource;
+		});
 		
 	  $provide.factory('Products', function($resource, TokenHandler){
 		    var resource = $resource('/assets/proxy/https://api.zanox.com/json/2011-03-01/programs/program/:programId' , {}, {
