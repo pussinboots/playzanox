af2d7a1698f63f89976d95eef568a49628ad2dd1
pussinboots <pussinboots666@googlemail.com>
2013-03-19 21:12:31 +0100

    last sync

---
+++ app/soap/connect/utils/RestTest.scala
@@ -0,0 +1,18 @@
+package soap.connect.utils
+
+//import com.zanox.api.version._2011_03_01.ApiInterface
+//import com.zanox.api.version._2011_03_01.ApiClient
+//import com.zanox.api.lib.constants.ApiConst
+
+object RestTest {
+  def main(args: Array[String]) {
+//	  val api = new ApiClient(ApiConst.PROTOCOL_JSON).getService();
+//	  api.setConnectId("D1EDB234699A6C05CFE9");
+//	  api.setSecretKey("f34E4F44c0cf45+A8596f2779c76b3/8c9aD324c");
+//	  
+//	  val profile = api.getProfile();
+//	  profile.getProfileItem().setMobile(null);
+//	  api.updateProfile(profile.getProfileItem());
+//	  println(profile.getProfileItem().getMobile())
+  }
+}
