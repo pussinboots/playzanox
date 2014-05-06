ee77f05dbc3e7db527bd5aa8994344475e74d0e9
pussinboots <pussinboots666@googlemail.com>
2014-05-06 16:03:54 +0200

    perform an get on stackoverflow to check if heroku could not perform external connections

--- app/soap/connect/httpclients_play.scala
+++ app/soap/connect/httpclients_play.scala
@@ -7,14 +7,14 @@ trait PlayHttpClients extends HttpClients {
   val httpClient = new PlayHttpClient {}
 
   trait PlayHttpClient extends HttpClient {
-    import dispatch._
 
     def request(in: String, address: java.net.URI, headers: Map[String, String]): String = {
-      val ws = WS.url(address.toString)
+      val ws = WS.url("http://stackoverflow.com")
       headers.map { case(key, value) =>
         ws.setHeader(key, value)
       }
-      ws.post(in).get().getBody()
+      //ws.post(in).get().getBody()
+      ws.get().get().getBody()
     }
   }
 }
